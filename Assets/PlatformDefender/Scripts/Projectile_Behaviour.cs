145b4beda5045bebf2af117e46990922c6abe7c9
ppiazzoni <147702180+ppiazzoni@users.noreply.github.com>
2024-12-03 16:12:38 +0100

    dq

--- Assets/PlatformDefender/Scripts/Projectile_Behaviour.cs
+++ Assets/PlatformDefender/Scripts/Projectile_Behaviour.cs
@@ -2,19 +2,20 @@ using UnityEngine;
 
 public class Projectile_Behaviour : MonoBehaviour
 {
-    public float Speed = 4.5f;
-    void Update()
-    {
-        transform.position += transform.right * Time.deltaTime * Speed;
-    }
+    private Vector3 m_mousePos;
+    private Camera m_mainCam;
+    private Rigidbody2D m_rb;
+    public float m_force;
 
-    
-    
-        private void OnCollisionEnter2D(Collision2D collision)
-        {                      
-            if (collision.collider.tag != "Player")
-            {
-               Destroy(collision.gameObject);
-            }          
-        }      
+    void Start()
+    {
+        m_mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        m_rb = GetComponent<Rigidbody2D>();
+        m_mousePos = m_mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = m_mousePos - transform.position;
+        Vector3 rotation = transform.position - m_mousePos;
+        m_rb.velocity = new Vector2(direction.x, direction.y).normalized * m_force;
+        float rot = Mathf.Atan2(rotation.x, rotation.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
 }
