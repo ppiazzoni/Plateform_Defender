2cd08518bfce6ab028f467ea01f5c52a56ac0d22
ppiazzoni <147702180+ppiazzoni@users.noreply.github.com>
2024-12-19 13:15:55 +0100

    ehg

--- Assets/PlatformDefender/Scripts/DamagePlayer.cs
+++ Assets/PlatformDefender/Scripts/DamagePlayer.cs
@@ -6,17 +6,25 @@ using UnityEngine;
 public class DamagePlayer : MonoBehaviour
 {
     public CharaController pHealth;
+    public CharaController targetScript;
+    public float m_burnKnockback = 80;
     public float m_damage;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            
             CharaController charaController = collision.GetComponentInParent<CharaController>();
             if (charaController)
-            {
-                print("cum");
-                pHealth.m_health -= m_damage;                
+            {                
+                pHealth.m_health -= m_damage;
+                m_charaController.Rb.AddForce(m_burnKnockback * Vector2.up, ForceMode2D.Impulse);
+            }
+            
+            if (pHealth.m_health < 0)
+            {               
+                targetScript.OnDeath();
             }
         }
     }
